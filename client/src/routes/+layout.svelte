5fa3b43128ec1d722850e5858187a30c7a42f7a6
nachop51 <nachoperalta0@gmail.com>
2025-06-18 16:16:18 +0400

    set up basic front libraries

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
+	import Navbar from '$lib/components/navbar.svelte';
 	import '../app.css';
+	import { ModeWatcher } from 'mode-watcher';
 
 	let { children } = $props();
 </script>
+
+<ModeWatcher />
+
+<Navbar />
 
 {@render children()}
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import type { ButtonSize } from '$lib/components/ui/button/button.svelte';
+	import { Button, type ButtonVariant } from '$lib/components/ui/button/index.js';
+
+	const variants = ['default', 'ghost', 'outline', 'secondary', 'link'] as ButtonVariant[];
+	const sizes = ['sm', 'md', 'lg', 'icon'] as ButtonSize[];
+</script>
+
+<Button variant="ghost">Tests</Button>
+
+<div class="grid grid-cols-8 justify-items-center text-center gap-4 bg-foreground/20 p-4 max-w-4xl">
+	{#each variants as variant (variant)}
+		{#each sizes as size (size)}
+			<Button {variant} {size}>GO</Button>
+
+			<span>
+				{variant}
+				{size}
+			</span>
+		{/each}
+	{/each}
+</div>
