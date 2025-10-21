994cf22d906ddd1ad46cdeb09a55604663a73169
nachop51 <nachoperalta0@gmail.com>
2025-10-21 23:27:36 +0400

    Update .gitignore to remove server/tmp/ and enhance db index.ts with type exports for User, Account, and Transaction

--- apps/db/index.ts
+++ apps/db/index.ts
@@ -1,4 +1,8 @@
 import { db } from './src'
 import { users, accounts, transactions } from './src/schema'
 
+export type User = typeof users.$inferSelect
+export type Account = typeof accounts.$inferSelect
+export type Transaction = typeof transactions.$inferSelect
+
 export { db, users, accounts, transactions }
